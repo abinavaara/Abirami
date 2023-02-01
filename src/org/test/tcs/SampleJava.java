5abb801b531b3419ed46439b51ec9b3ca827e858
abinavaara <abinavaara@gmail.com>
2023-02-01 13:39:48 +0530

    3rd proj

---
+++ src/org/test/tcs/SampleJava.java
@@ -0,0 +1,19 @@
+package org.test.tcs;
+
+public class SampleJava {
+private void empId() {
+	System.out.println("done");
+
+}
+private void empName() {
+	System.out.println("not done");
+
+}
+	public static void main(String[] args) {
+
+SampleJava e = new SampleJava();
+		e.empId();
+		e.empName();
+	}
+
+}
